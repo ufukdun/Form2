061c3e5f09ba9c0465916e29591a033bce768778
ufuk dundar <ufukdundar158@hotmail.com>
2016-11-27 17:10:31 +0200

    nvccgc

--- Form2/Form1.cs
+++ Form2/Form1.cs
@@ -16,5 +16,37 @@ namespace Form2
         {
             InitializeComponent();
         }
+        Random random = new Random();
+        int red, green, blue;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                System.Drawing.Color renk = RenkOlustur();
+                Button btn = new Button();
+                btn.Width = 30;
+                btn.Height = 30;
+                btn.BackColor = renk;
+                btn.Left = 100 + (btn.Width * i);
+                btn.Tag = renk;
+                btn.Click += btn_Click;
+                this.Controls.Add(btn);
+            }
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button secilenBtn = (Button)sender;
+            this.BackColor = (System.Drawing.Color)secilenBtn.BackColor;
+        }
+        private System.Drawing.Color RenkOlustur()
+        {
+            red = random.Next(0, 256);
+            green = random.Next(0, 256);
+            blue = random.Next(0, 256);
+
+            System.Drawing.Color randomRenk = System.Drawing.Color.FromArgb(red, green, blue);
+            return randomRenk;
+        }
     }
 }
